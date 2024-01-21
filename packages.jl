c2dde0a4238005d68211ffb443f3317d72669b89
zhenyue <zhenyue.fu@etu.upmc.fr>
2024-01-21 16:17:04 +0100

    Add and update packages

--- packages.jl
+++ packages.jl
@@ -1,10 +1,11 @@
 import Pkg
 
-Pkg.add("Gurobi")
+# Pkg.add("Gurobi")
 # Pkg.update("CPLEX")
-# Pkg.add("COPT")
+Pkg.add("COPT")
 Pkg.add("MultiObjectiveAlgorithms")
 Pkg.add("JuMP")
 Pkg.add("Distances")
 Pkg.add("StatsBase")
 Pkg.add("Combinatorics")
+Pkg.add("JLD2")
